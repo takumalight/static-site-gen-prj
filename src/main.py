b47c52b35849fc1056d412f59d86b63884ce0af5
takumalight <martianburritos@gmail.com>
2025-05-21 15:17:49 -0500

    CH5-L4

--- src/main.py
+++ src/main.py
@@ -33,11 +33,8 @@ def copy_recursively(src, dst):
             print(f"Copying file: {item}")
             shutil.copy(src_path, dst_path)
 
-def extract_title(markdown_file):
-    f = open(markdown_file, 'r')
-    markdown_text = f.read()
-    f.close()
-    for line in markdown_text.splitlines():
+def extract_title(markdown):
+    for line in markdown.splitlines():
         if line.startswith('# '):
             title = line[1:].strip()
             if title:
@@ -54,23 +51,45 @@ def generate_page(from_path, template_path, dest_path):
     template_text = tp.read()
     tp.close()
 
+    title = extract_title(markdown_text)
     html_text = markdown_to_html_node(markdown_text).to_html()
-    title = extract_title(from_path)
 
     template_text = template_text.replace("{{ Title }}", title)
     template_text = template_text.replace("{{ Content }}", html_text)
 
+    if not os.path.exists(os.path.dirname(dest_path)):
+        os.makedirs(os.path.dirname(dest_path))
+
     with open(dest_path, 'w') as f:
         f.write(template_text)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for item in os.listdir(dir_path_content):
+        item_path = os.path.join(dir_path_content, item)
+        if os.path.isdir(item_path):
+            new_dest_dir = os.path.join(dest_dir_path, item)
+            if not os.path.exists(new_dest_dir):
+                os.makedirs(new_dest_dir)
+            generate_pages_recursive(item_path, template_path, new_dest_dir)
+        elif item.endswith('.md'):
+            dest_file = os.path.splitext(item)[0] + '.html'
+            dest_path = os.path.join(dest_dir_path, dest_file)
+            generate_page(item_path, template_path, dest_path)
 
     
 
 def main():
     copy_static_files()
-    generate_page(
-        os.path.join(os.path.dirname(__file__), '..', 'content', 'index.md'),
+    # generate_page(
+    #     os.path.join(os.path.dirname(__file__), '..', 'content', 'index.md'),
+    #     os.path.join(os.path.dirname(__file__), '..', 'template.html'),
+    #     os.path.join(os.path.dirname(__file__), '..', 'public', 'index.html')
+    # )
+    generate_pages_recursive(
+        os.path.join(os.path.dirname(__file__), '..', 'content'),
         os.path.join(os.path.dirname(__file__), '..', 'template.html'),
-        os.path.join(os.path.dirname(__file__), '..', 'public', 'index.html')
+        os.path.join(os.path.dirname(__file__), '..', 'public')
     )
+
 
 main()
